505fd742ba371f487dab47a70ce820eface5620f
gom201601 <mason201601@gmail.com>
2026-08-30 22:35:35 +0900

    fix : ln_half logic fixed

--- src/peft/ln_half.py
+++ src/peft/ln_half.py
@@ -1,16 +1,17 @@
 from torch import nn
-
+import re
 
 def mark_only_half_layernorm_as_trainable(clip_model):
     clip_model.requires_grad_(False)
 
-    for encoder in (clip_model.vision_model, clip_model.text_model):
-        idx = 0
-        for module in encoder.modules():
-            if isinstance(module, nn.LayerNorm):
-                if idx % 2 == 1:
-                    module.requires_grad_(True)
 
-                idx += 1
+    for encoder in (clip_model.vision_model, clip_model.text_model):
+        for name, module in encoder.named_modules():
+            if isinstance(module, nn.LayerNorm):                    
+                m = re.search(r"layers\.(\d+)\.", name)
+
+                if m and int(m.group(1)) % 2 == 1:
+                    module.requires_grad_(True)
+                    print(name)
 
     return [parameter for parameter in clip_model.parameters() if parameter.requires_grad]
